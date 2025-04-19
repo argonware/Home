a2d0173c1d2a64adc73984f4e765e9eafdc111a7
AronWare <argonware.official@gmail.com>
2025-04-19 11:21:26 +0500

    files has been added to the repo

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 function particles(e) {
-    for (let i = 0; i < 20; i++) {
-    const div = document.createElement("div").className = "particle";
+  for (let i = 0; i < 20; i++) {
+    const div = document.createElement("div");
     e.appendChild(div);
-    e.style.position = "relative";
+    div.classList.add("particle");
     div.style.width = `${Math.random() * 10 + 5}px`;
     div.style.position = "absolute";
-    div.style.height = `${Math.random() * 10 + 5}px`;
+    div.style.aspectRatio = "1/1";
     div.style.borderRadius = "50%";
-    div.style.transform = `translateX(${Math.floor(Math.random() * 100)}%)`;
-    div.style.backgroundColor = `var(--primary-color${Math.floor(Math.random() * 5)})`;
+    div.style.transform = `translateX(${Math.floor(Math.random() * 70)}%)`;
+    div.style.backgroundColor = `var(--primary-color${Math.floor(
+      Math.random() * 5
+    )})`;
     div.style.left = `${Math.random() * 100}vw`;
-    div.style.top = `${Math.random() * 100}vh`;
-   div.style.animation = `particle ${Math.random() * 2 + 1}s linear forwards`;
-    }
+    div.style.animation = `particle 5s ${Math.floor(
+      Math.random() * 10
+    )}s infinite linear`;
+    div.style.opacity = 0;
+  }
+  return e;
 }
+
+document.querySelectorAll(".particled").forEach((e) => {
+  particles(e);
+});
